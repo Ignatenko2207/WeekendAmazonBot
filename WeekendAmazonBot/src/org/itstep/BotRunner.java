cb436aa4db8e0003daba49c0f356c6bd79c53825
Alex <ignatenko2207@gmail.com>
2018-02-25 08:59:36 +0200

    Add registration method

--- WeekendAmazonBot/src/org/itstep/BotRunner.java
+++ WeekendAmazonBot/src/org/itstep/BotRunner.java
@@ -1,5 +1,6 @@
 package org.itstep;
 
+import org.itstep.model.Account;
 import org.itstep.service.Timer;
 import org.itstep.service.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -11,8 +12,9 @@ public class BotRunner {
 		WebDriverManager manager = new WebDriverManager();
 		WebDriver driver = manager.getWebDriver();
 		
+		Account account = new Account("Alex", "Ignatenko", "Ignatenko_Alex_123456789@gmail.com", "123456789");
 		
-		
+		driver = manager.registerAccount(driver, account);
 		
 		driver.quit();
 
